b759af038cb64e5c7c6609285cdd3ceea2437a8e
nintendo-mini <carlos@verold.com>
2016-10-24 22:53:08 +0200

    removes polyfills

--- js/find-polyfill.js
+++ js/find-polyfill.js
@@ -1,11 +1,18 @@
 'use strict';
 
-module.exports = function getPolyfill() {
-	// Detect if an implementation exists
-	// Detect early implementations which skipped holes in sparse arrays
-	var implemented = Array.prototype.find && [, 1].find(function (item, index) {
-		return index === 0;
-	});
-
-	return implemented ? Array.prototype.find : require('./implementation');
-};
+Array.prototype.find = Array.prototype.find || function(callback) {
+  if (this === null) {
+    throw new TypeError('Array.prototype.find called on null or undefined');
+  } else if (typeof callback !== 'function') {
+    throw new TypeError('callback must be a function');
+  }
+  var list = Object(this);
+  // Makes sures is always has an positive integer as length.
+  var length = list.length >>> 0;
+  var thisArg = arguments[1];
+  for (var i = 0; i < length; i++) {
+    var element = list[i];
+    if ( callback.call(thisArg, element, i, list) ) {
+      return element;
+    }
+  }
